4fcbff93ea6a198cb71213363ca7f0fb07c75f3d
Krishnan Nair <krishnannair12@gmail.com>
2021-02-25 21:43:21 -0500

    added vaccine iframe

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import ChartistGraph from 'react-chartist'
 import Jumbotron from './Jumbotron'
+// import { Dollar } from 'react-icons/AiOutlineDollarCircle';
+// import { Calendar } from 'react-icons/AiOutlineCalendar';
+// import { Location } from 'react-icons/GoLocation';
 
 class Dashboard extends Component {
   render() {
@@ -13,12 +16,15 @@ class Dashboard extends Component {
           </div>
           <div className="row">
             <div className="col-md-4">
+              {/* <Calendar /> */}
               <p>Upon entering your start and end destination, our system gathers fare data from te </p>
             </div>
             <div className="col-md-4">
+              {/* <Location /> */}
               <p>Step 2</p>
             </div>
             <div className="col-md-4">
+              {/* <Dollar /> */}
               <p>Step 3: We compare prices to Uber and Lyft</p>
             </div>
           </div>
