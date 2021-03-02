220e07474083573ebe8c6327de313e816b3d61e6
Krishnan Nair <krishnannair12@gmail.com>
2021-03-02 18:52:39 -0500

    updated query page

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -31,58 +31,66 @@ class Charts extends Component {
       ]
     }
     return (
-        <div className="content">
+        <div className="content border2">
             <div className="container-fluid">
                 <div className="row">
                     < Query />
                 </div>
-                <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-                    <Row>
-                        <Col sm={3}>
-                        <Nav variant="pills" className="flex-column">
-                            <Nav.Item>
-                            <Nav.Link eventKey="first">Price Change Since 2018</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                            <Nav.Link eventKey="second">View Real Time NYC COVID Data</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                        </Col>
-                        <Col sm={9}>
-                        <Tab.Content>
-                            <Tab.Pane eventKey="first">
-                            <div className="col-md-8">
-                                <div className="card">
-                                    <div className="card-header ">
-                                        <h4 className="card-title">Users Behavior</h4>
-                                        <p className="card-category">24 Hours performance</p>
-                                    </div>
-                                    <div className="card-body ">
-                                        <ChartistGraph data={dataSales} type="Line" />
-                                    </div>
-                                    <div className="card-footer ">
-                                        <div className="legend">
-                                            <i className="fa fa-circle text-info"></i> Open
-                                            <i className="fa fa-circle text-danger"></i> Click
-                                            <i className="fa fa-circle text-warning"></i> Click Second Time
+                <div className="row">
+                    <h2 className="page-header">
+                        Your estimated fare is <span id="estimated-fare"></span>
+                    </h2>
+                </div><br></br>
+                <div>
+                    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                        <Row>
+                            <Col sm={3}>
+                            <Nav variant="pills" className="flex-column">
+                                <Nav.Item>
+                                <Nav.Link eventKey="first">Price Change Since 2018</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                <Nav.Link eventKey="second">View Real Time NYC COVID Data</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            </Col>
+                            <Col sm={9}>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                <div className="col-md-8">
+                                    <div className="card">
+                                        <div className="card-header ">
+                                            <h4 className="card-title">Users Behavior</h4>
+                                            <p className="card-category">24 Hours performance</p>
                                         </div>
-                                        <hr />
-                                        <div className="stats">
-                                            <i className="fa fa-history"></i> Updated 3 minutes ago
+                                        <div className="card-body ">
+                                            <ChartistGraph data={dataSales} type="Line" />
+                                        </div>
+                                        <div className="card-footer ">
+                                            <div className="legend">
+                                                <i className="fa fa-circle text-info"></i> Open
+                                                <i className="fa fa-circle text-danger"></i> Click
+                                                <i className="fa fa-circle text-warning"></i> Click Second Time
+                                            </div>
+                                            <hr />
+                                            <div className="stats">
+                                                <i className="fa fa-history"></i> Updated 3 minutes ago
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            {/* <Sonnet /> */}
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="second">
-                                <iframe src="https://public.tableau.com/views/COVID-19VaccinationTracker/Geography?:language=en&:embed=y&:embed_code_version=3&:loadOrderID=0&:display_count=y&:origin=viz_share_link?:showVizHome=no&:embed=true" width="705" height="1385"></iframe>
-                            {/* <Sonnet /> */}
-                            </Tab.Pane>
-                        </Tab.Content>
-                        </Col>
-                    </Row>
-                </Tab.Container>
+                                {/* <Sonnet /> */}
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <iframe src="https://public.tableau.com/views/COVID-19VaccinationTracker/Geography?:language=en&:embed=y&:embed_code_version=3&:loadOrderID=0&:display_count=y&:origin=viz_share_link?:showVizHome=no&:embed=true" width="705" height="1385"></iframe>
+                                {/* <Sonnet /> */}
+                                </Tab.Pane>
+                            </Tab.Content>
+                            </Col>
+                        </Row>
+                    </Tab.Container>
+                </div>
+                
                     {/* <div className="col-md-4">
                         <div className="card ">
                             <div className="card-header ">
--- src/components/Query.js
+++ src/components/Query.js
@@ -6,37 +6,40 @@ class Query extends Component {
     return (
         <div>
             <h4 className="display-4 page-header">Get your rate estimate by entering your information below</h4> 
-            <div className="card">
+            <div className="card" id="query-card">
                 <div className="card-body">
-                    <form className = "form-row" id="query-selection">
-                        <div className="col-md-3 text-center">
-                            <label for="pickup">Pickup Zipcode:</label>
-                            <input type="text" size="6" id="pickup" name="pickup" placeholder="Enter here"></input>
+                    <form id="query-selection">
+                        <div className = "form-row justify-content-center">
+                            <div className="col-md-3 text-center">
+                                <label for="pickup">Pickup Zipcode:</label>
+                                <input type="text" size="6" id="pickup" name="pickup" placeholder="Enter here"></input>
+                            </div>
+                            <div className="col-md-3 text-center">
+                                <label for="dropoff">Dropoff Zipcode:</label>
+                                <input type="text" size="6" id="dropoff" name="dropoff" placeholder="Enter here"></input>                        
+                            </div>
+                            <div className="col-md-4 text-center">
+                                <label for="month">Select Month of Travel:</label>
+                                <select class="form-select" length="2" id="month" name="month">
+                                    <option value="Jan">January</option>
+                                    <option value="Feb">February</option>
+                                    <option value="March">March</option>
+                                    <option value="April">April</option>
+                                    <option value="May">May</option>
+                                    <option value="June">June</option>
+                                    <option value="July">July</option>
+                                    <option value="August">August</option>
+                                    <option value="September">September</option>
+                                    <option value="October">October</option>
+                                    <option value="November">November</option>
+                                    <option value="December">December</option>
+                                </select>  
+                            </div>
                         </div>
-                        <div className="col-md-3 text-center">
-                            <label for="dropoff">Dropoff Zipcode:</label>
-                            <input type="text" size="6" id="dropoff" name="dropoff" placeholder="Enter here"></input>                        
-                        </div>
-                        <div className="col-md-4 text-center">
-                            <label for="month">Select Month of Travel:</label>
-                            <select class="form-select" length="2" id="month" name="month">
-                                <option value="Jan">January</option>
-                                <option value="Feb">February</option>
-                                <option value="March">March</option>
-                                <option value="April">April</option>
-                                <option value="May">May</option>
-                                <option value="June">June</option>
-                                <option value="July">July</option>
-                                <option value="August">August</option>
-                                <option value="September">September</option>
-                                <option value="October">October</option>
-                                <option value="November">November</option>
-                                <option value="December">December</option>
-                            </select>  
-                        </div>
-                        <div className="col-md-2 text-center">
-                            <button type="submit">Get my estimate</button>   
-                        </div>
+                       
+                    <div className="form-row py-3 justify-content-center">
+                        <button id="query-submit" type="submit">Get my estimate</button>   
+                    </div>
                     </form>
                 </div>
             </div>    
